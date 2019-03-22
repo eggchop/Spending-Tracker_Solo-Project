0a8ca57b4c5c301d2b17cd4a80bf77620ac2f70e
Charlie Walker <charliewalker2016@outlook.com>
2019-03-22 21:55:47 +0000

    added datetime stamp

--- db/frugal.sql
+++ db/frugal.sql
@@ -2,21 +2,20 @@ DROP TABLE merchants;
 DROP TABLE tags;
 DROP TABLE transactions;
 
-CREATE TABLE merchants(
+CREATE TABLE IF NOT EXISTS merchants(
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255)
 );
 
-CREATE TABLE tags(
+CREATE TABLE IF NOT EXISTS tags(
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255)
 );
 
-CREATE TABLE transactions(
+CREATE TABLE IF NOT EXISTS transactions(
   id SERIAL8 PRIMARY KEY,
   price FLOAT8,
-  -- date_added TIMESTAMP DEFAULT NULL,
-  date_added VARCHAR(255),
+  date_added TIMESTAMP,
   tag_id INT8 REFERENCES tags(id) ON DELETE CASCADE,
   merchant_id INT8 REFERENCES merchants(id) ON DELETE CASCADE
 );
--- models/transaction.rb
+++ models/transaction.rb
@@ -1,11 +1,12 @@
 require_relative('../db/sql_runner')
+require 'Time'
 
 class Transaction
-  attr_reader :id,:date_added, :price
+  attr_reader :id, :price, :date_added
   attr_accessor :tag_id, :merchant_id
   def initialize(options)
     @id = options['id'].to_i if options['id']
-    @date_added = options['date_added']
+    @date_added = options['date_added'] if options['date_added']
     @price = options['price'].to_f
     @tag_id = options['tag_id'].to_i
     @merchant_id = options['merchant_id'].to_i
@@ -17,11 +18,13 @@ class Transaction
   end
 
   def save()
-    sql = "INSERT INTO transactions (date_added,price,tag_id,merchant_id) VALUES ($1,$2,$3,$4)RETURNING id"
-    values = [@date_added,@price,@tag_id,@merchant_id]
+    sql = "INSERT INTO transactions (date_added,price,tag_id,merchant_id) VALUES ('#{Time.now()}',$1,$2,$3)RETURNING id,date_added"
+    values = [@price,@tag_id,@merchant_id]
     result = SqlRunner.run(sql, values)
     id = result.first['id']
+    date_added = result.first['date_added']
     @id = id
+    @date_added = Time.parse(date_added)
   end
 
   def self.all
@@ -30,8 +33,8 @@ class Transaction
   end
 
   def update
-    sql = "UPDATE transactions SET (date_added,price,tag_id,merchant_id) = ($1,$2,$3,$4) WHERE id = $5"
-    values = [@date_added,@price,@tag_id,@merchant_id,@id]
+    sql = "UPDATE transactions SET (price,tag_id,merchant_id) = ($1,$2,$3) WHERE id = $4"
+    values = [@price,@tag_id,@merchant_id,@id]
     SqlRunner.run(sql,values)
   end
 
@@ -70,5 +73,25 @@ class Transaction
     values = [merchant_name.downcase]
     return SqlRunner.run(sql, values).map{|hash| Transaction.new(hash)}
   end
+
+  # def self.find_by_month
+  #   sql = "SELECT *
+  #         FROM transactions
+  #         JOIN merchants
+  #         ON merchants.id = transactions.merchant_id
+  #         WHERE merchants.name = $1
+  #         ORDER BY transactions.date_added"
+  #   values = [merchant_name.downcase]
+  #   return SqlRunner.run(sql, values).map{|hash| Transaction.new(hash)}
+  # end
+
+  def self.total
+    sql = "SELECT SUM(transactions.price) AS total
+          FROM transactions"
+    return SqlRunner.run(sql).first['total'].to_f
+  end
+
+  # def convert_sql_datetime_to_ruby_date_object
+
 
 end
